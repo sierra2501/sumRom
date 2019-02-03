95750c766ff7104facebb23a7213dc717422d23c
sierra <hoge>
2019-02-03 23:39:47 +0900

    ファイルの追加

---
+++ README.md
@@ -0,0 +1,18 @@
+# sumRom.py
+Shade3Dのプラグイン。選択した稜線の長さの合計を求める。
+
+## 使い方
+1. Shade3Dの『メインメニュー』の『表示』より『スクリプト』をクリックします。
+2. 表示されたウインドウにソースコードをコピーもしくは『読込』ボタンをクリックしファイルを指定します。
+3. 『実行』ボタンをクリックすると実行されます。
+
+## バージョン
+-v 1.0.0
+
+## ライセンス
+[CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/)
+
+## 作者
+sierra  
+- [Wordpress](http://tenteroring.luna.ddns.vc/sierra/)  
+- [Twitter](https://twitter.com/sierra2501?lang=ja)
---
+++ sumRom.py
@@ -0,0 +1,37 @@
+import math
+
+def sumRom():
+
+    count = 0
+    r = 0
+
+    # 稜線数
+    edges_num = xshade.scene().active_shape().number_of_edges
+
+    while count < edges_num:
+
+        if xshade.scene().active_shape().edge(count).active == True:
+        
+            # 頂点のインデックス値
+            edge_v0 = xshade.scene().active_shape().edge(count).v0
+            edge_v1 = xshade.scene().active_shape().edge(count).v1
+
+            #  頂点の座標
+            vertex_v0 =  xshade.scene().active_shape().vertex(edge_v0).position
+            vertex_v1 = xshade.scene().active_shape().vertex(edge_v1).position
+
+            #  長さ
+            r = math.sqrt((vertex_v0[0] - vertex_v1[0]) ** 2 + (vertex_v0[1] - vertex_v1[1]) ** 2 + (vertex_v0[2] - vertex_v1[2]) ** 2) + r
+    
+        else:
+            r = 0 + r
+
+        count = count + 1
+
+    return r
+
+def main():
+
+    print "R = " + str(sumRom())
+
+main()
